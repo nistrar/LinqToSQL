a7d625f819faa6d504c58dfcba0df3783133e0f2
Dan Costea <costea.dan.cristian@gmail.com>
2021-09-23 20:31:02 +0200

    CRUD with LINQ on SQL

--- LinqToSQL/MainWindow.xaml.cs
+++ LinqToSQL/MainWindow.xaml.cs
@@ -21,14 +21,154 @@ namespace LinqToSQL
     /// </summary>
     public partial class MainWindow : Window
     {
+        LinqToSQLDataClassesDataContext dataContext;
         public MainWindow()
         {
-            LinqToSQLDataClassesDataContext dataContext;
+            
             InitializeComponent();
 
             string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.linqDBConnectionString"].ConnectionString;
 
             dataContext = new LinqToSQLDataClassesDataContext(connectionString);
+            //dataContext.ExecuteCommand("delete from student");
+
+            //InsertUniversities();
+            //InsertStudents();
+            //InsertLectures();
+            //InsertStudentLectureAssociations();
+            //GetUniversityofDan();
+
+            //GetLecturesFromDan();
+            //GetAllLecturesFromBeijing();
+            //UpdateDan();
+            DeleteEma();
+        }
+
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("delete from university");
+            
+            University yale = new University();
+            yale.Name = "Yale";
+            dataContext.Universities.InsertOnSubmit(yale);
+
+            University beijing = new University();
+            beijing.Name = "Beijing";
+            dataContext.Universities.InsertOnSubmit(beijing);
+
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+
+        public void InsertStudents()
+        {
+            University yale = dataContext.Universities.First(un => un.Name.Equals("Yale"));
+            University beijing = dataContext.Universities.First(un => un.Name.Equals("Beijing"));
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Vlad", Gender = "male", UniversityID = yale.Id });
+            students.Add(new Student { Name = "Dan", Gender = "male", UniversityID = yale.Id });
+            students.Add(new Student { Name = "Ema", Gender = "female", UniversityID = beijing.Id });
+
+            dataContext.Students.InsertAllOnSubmit(students);
+
+            dataContext.SubmitChanges();
+            
+            MainDataGrid.ItemsSource = dataContext.Students;
+        }
+
+        public void InsertLectures()
+        {
+            List<Lecture> lectures = new List<Lecture>();
+
+            lectures.Add(new Lecture { Name = "Mathematics" });
+            lectures.Add(new Lecture { Name = "Thermodynamics" });
+            lectures.Add(new Lecture { Name = "Information Technology" });
+
+            dataContext.Lectures.InsertAllOnSubmit(lectures);
+
+            dataContext.SubmitChanges();
+        }
+
+        public void InsertStudentLectureAssociations()
+        {
+            Student vlad = dataContext.Students.First(s => s.Name.Equals("Vlad"));
+            Student dan = dataContext.Students.First(s => s.Name.Equals("Dan"));
+            Student ema = dataContext.Students.First(s => s.Name.Equals("Ema"));
+
+            Lecture math = dataContext.Lectures.First(l => l.Name.Equals("Mathematics"));
+            Lecture thermo = dataContext.Lectures.First(l => l.Name.Equals("Thermodynamics"));
+            Lecture informatics = dataContext.Lectures.First(l => l.Name.Equals("Information Technology"));
+
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = vlad, Lecture = math });
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = dan, Lecture = thermo });
+
+            StudentLecture sEma = new StudentLecture();
+            sEma.StudentID = ema.Id;
+            sEma.LectureID = informatics.Id;
+            dataContext.StudentLectures.InsertOnSubmit(sEma);
+
+            dataContext.SubmitChanges();
+
+
+            MainDataGrid.ItemsSource = dataContext.StudentLectures;
+
+        }
+
+        public void GetUniversityofDan()
+        {
+            Student dan = dataContext.Students.First(s => s.Name.Equals("Dan"));
+            University dansUni = dan.University;
+
+            List<University> universities = new List<University>();
+
+            universities.Add(dansUni);
+            
+            MainDataGrid.ItemsSource = universities;
+
+        }
+
+        public void GetLecturesFromDan()
+        {
+            Student dan = dataContext.Students.First(s => s.Name.Equals("Dan"));
+
+            var dansLectures = from sl in dan.StudentLectures select sl.Lecture;
+
+            MainDataGrid.ItemsSource = dansLectures;
+        }
+        
+        public void GetAllLecturesFromBeijing()
+        {
+            var lecturesFromBeijing = from sl in dataContext.StudentLectures
+                                      join student in dataContext.Students
+                                      on sl.StudentID equals student.Id
+                                      where student.University.Name == "Yale"
+                                      select sl.Lecture;
+
+            MainDataGrid.ItemsSource = lecturesFromBeijing;
+        }
+
+        public void UpdateDan()
+        {
+            Student dan = dataContext.Students.FirstOrDefault(s => s.Name.Equals("Dan"));
+            dan.Name = "Costea";
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
+        }
+
+        public void DeleteEma()
+        {
+            Student ema = dataContext.Students.FirstOrDefault(s => s.Name.Equals("Ema"));
+
+            dataContext.Students.DeleteOnSubmit(ema);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
 
         }
     }
--- LinqToSQL/Properties/Settings.Designer.cs
+++ LinqToSQL/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace LinqToSQL.Properties {
                 return ((string)(this["linqDBConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-J7689UF;Initial Catalog=udemy_tutorial;Persist Security Info=" +
+            "True;User ID=sa")]
+        public string udemy_tutorialConnectionString {
+            get {
+                return ((string)(this["udemy_tutorialConnectionString"]));
+            }
+        }
     }
 }
